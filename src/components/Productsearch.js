7636c40bb9ade31b5ad1b61ee703f6eda6ce8bc5
truptigaonkar <trups11.trupti@gmail.com>
2020-01-17 11:04:15 +0100

    Created folder components with component Productsearch

---
+++ src/components/Productsearch.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+
+class Productsearch extends Component {
+    render() {
+        return (
+            <div>
+                <Helmet><title>Product Search</title></Helmet>
+                <h3>Product Search</h3>
+            </div>
+        )
+    }
+}
+
+export default Productsearch;
