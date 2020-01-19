7cec4c12ba1aeddf6389c1e1dd5fe435de76120e
truptigaonkar <trups11.trupti@gmail.com>
2020-01-19 09:23:53 +0100

    Updates

---
+++ precache-manifest.cca853d5a4674e5e77918eefcbf3d9cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de4b715404e4ed10d80a36fca175c17",
+    "url": "/product-search/index.html"
+  },
+  {
+    "revision": "575c4c190a59dc8aed3f",
+    "url": "/product-search/static/css/main.965c1ab5.chunk.css"
+  },
+  {
+    "revision": "fe68518761645a5a0c25",
+    "url": "/product-search/static/js/2.b00b3adb.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/product-search/static/js/2.b00b3adb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "575c4c190a59dc8aed3f",
+    "url": "/product-search/static/js/main.da01a5ec.chunk.js"
+  },
+  {
+    "revision": "5b6253169214371f960c",
+    "url": "/product-search/static/js/runtime-main.b0705fd2.js"
+  }
+]);
